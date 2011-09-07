254ad78259fc447467158efcb2f1e8fbf682e3ab
Andrea Fabrizi <andrea.fabrizi@gmail.com>
2011-09-07 09:13:21 +0200

    Version 0.8.1

--- dropbox_uploader.sh
+++ dropbox_uploader.sh
@@ -1,26 +1,66 @@
 #!/bin/bash
 #
-# Dropbox Uploader Script v0.7.1
+# Dropbox Uploader Script v0.8.1
 #
 # Copyright (C) 2010-2011 Andrea Fabrizi <andrea.fabrizi@gmail.com>
-#  
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#  
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#  
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# CHANGELOG:
+#
+# Version 0.8.1 - 31/08/2011
+# Modified by Dawid Ferenczy (www.ferenczy.cz)
+#  - added prompt for the Dropbox password from keyboard, if there is no password
+#    hardcoded or given as script command line parameter (interactive mode)
+#  - added INTERACTIVE_MODE variable - when set to 1 show CURL progress bar.
+#    Set to 1 automatically when there is no password hardcoded or given as
+#    parameter. Controls verbosity of CURL.
+#
+# Version 0.7.1 - 10/03/2011:
+# - Minor bug fixes
+# 
+# Version 0.7 - 10/03/2011:
+# - New command line interface
+# - Code clean
+#
+# Version 0.6 - 11/01/2011:
+# - Fixed issue with spaces in file/forder name
+#
+# Version 0.5 - 04/01/2011:
+# - Recursive directory upload
+#
+# Version 0.4 - 29/12/2010:
+# - Now works on BSD and MAC
+# - Interactive prompt for username and password
+# - Speeded up the uploading process
+# - Debug mode
+#
+# Version 0.3 - 18/11/2010:
+# - Regex updated
+#
+# Version 0.2 - 04/09/2010:
+# - Removed dependencies from tempfile
+# - Code clean
+#
+# Version 0.1 - 23/08/2010:
+# - Initial release
+#
 
 #DROPBOX ACCOUNT
 #For security reasons, it is not recommended to modify this script
-#to hardcode a login and password.  However, this can be done if 
+#to hardcode a login and password.  However, this can be done if
 #automation is necessary.
 LOGIN_EMAIL=""
 LOGIN_PASSWD=""
@@ -45,7 +85,11 @@ UPLOAD_URL="https://dl-web.dropbox.com/upload"
 COOKIE_FILE="/tmp/du_cookie_$RANDOM"
 RESPONSE_FILE="/tmp/du_resp_$RANDOM"
 BIN_DEPS="curl sed grep tr pwd"
-VERSION="0.7.1"
+VERSION="0.8.1"
+
+#Set to 1 to show CURL progress bar. Sets automatically to 1 when there is no password
+#hardcoded or given as parameter. Controls verbosity of CURL.
+INTERACTIVE_MODE=0
 
 if [ $DEBUG -ne 0 ]; then
     set -x
@@ -76,7 +120,7 @@ function remove_temp_files
 function get_token
 {
     TOKEN=$(cat $1 | tr -d '\n' | sed 's/.*<form action="'$2'"[^>]*>\s*<input type="hidden" name="t" value="\([a-z 0-9]*\)".*/\1/')
-    echo $TOKEN      
+    echo $TOKEN
 }
 
 #Upload a single file to dropbox
@@ -86,7 +130,7 @@ function dropbox_upload
 {
     UPLOAD_FILE=$1
     DEST_FOLDER=$2
-    
+
     print " > Uploading '$UPLOAD_FILE' to 'DROPBOX$DEST_FOLDER'..."
     curl -s -i -b $COOKIE_FILE -o $RESPONSE_FILE -F "plain=yes" -F "dest=$DEST_FOLDER" -F "t=$TOKEN" -F "file=@$UPLOAD_FILE"  "$UPLOAD_URL"
     grep "HTTP/1.1 302 FOUND" "$RESPONSE_FILE" > /dev/null
@@ -107,17 +151,17 @@ function dropbox_upload
 function dropbox_upload_dir
 {
     for i in *; do
-          
+
         if [ -f "$i" ]; then
             dropbox_upload "$i" "$1"
         fi
-        
+
         if [ -d "$i" ]; then
             local OLD_PWD=$(pwd)
             cd "$i"
             dropbox_upload_dir "$1/$i"
             cd "$OLD_PWD"
-        fi        
+        fi
     done
 }
 
@@ -177,13 +221,26 @@ while getopts "u:p:f:d:v" opt; do
         d)
             DEST_FOLDER="$OPTARG"
             let optn++;;
-        v)	
+        v)
             VERBOSE=1;;
-        *)	
+        *)
             usage;
             exit 0;
     esac
 done
+
+# interactive mode - prompt for the Dropbox password, if not hardcoded or given as parameter
+if [ "$LOGIN_PASSWD" == "" ]; then
+	read -s -p "Password: " LOGIN_PASSWD
+	echo
+	INTERACTIVE_MODE=1
+fi
+
+if [ $INTERACTIVE_MODE == 1 ]; then
+	CURL_PARAMETERS="--progress-bar"
+else
+	CURL_PARAMETERS="-s --show-error"
+fi
 
 if [ $optn -lt 1 ] || [ "$LOGIN_EMAIL" == "" ] || [ "$LOGIN_PASSWD" == "" ]; then
 	usage;
@@ -228,7 +285,7 @@ fi
 
 #LOGIN
 print " > Logging in..."
-curl -s -i -c $COOKIE_FILE -o $RESPONSE_FILE --data "login_email=$LOGIN_EMAIL&login_password=$LOGIN_PASSWD&t=$TOKEN" "$LOGIN_URL"
+curl $CURL_PARAMETERS -i -c $COOKIE_FILE -o $RESPONSE_FILE --data "login_email=$LOGIN_EMAIL&login_password=$LOGIN_PASSWD&t=$TOKEN" "$LOGIN_URL"
 grep "location: /home" $RESPONSE_FILE > /dev/null
 
 if [ $? -ne 0 ]; then
