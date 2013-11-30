6a4a342c70cbbaa89f78c30fbadf42f8cee0eb9f
Paul Reny <paultreny@gmail.com>
2013-11-30 01:31:13 -0500

    clarified how to install greadlink (via homebrew) for Mac OS X

--- dropShell.sh
+++ dropShell.sh
@@ -30,8 +30,8 @@ else
     fi
 fi
 
-#For MaxOSX install readlink using Homebrew: http://brew.sh/
-#brew install readlink
+# For MaxOSX install greadlink using Homebrew: http://brew.sh
+# Install coreutils (which includes greadlink) ->$ brew install coreutils
 if [ "${OSTYPE:0:6}" == "darwin" ]; then
     READLINK="greadlink"
 else
