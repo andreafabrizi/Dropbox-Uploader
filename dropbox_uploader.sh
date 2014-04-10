e09424ebcc2cff2b995d06883b9eb28c66232ad4
Sam Jacobs <samuel@ijacobs.co.uk>
2014-04-10 23:25:28 +0100

    Allow environment variable to specify config file
    DROPBOX_UPLOADER_CONFIG_FILE can be used to specify the location of a configuration file.

--- dropbox_uploader.sh
+++ dropbox_uploader.sh
@@ -20,7 +20,7 @@
 #
 
 #Default configuration file
-CONFIG_FILE=~/.dropbox_uploader
+CONFIG_FILE=${DROPBOX_UPLOADER_CONFIG_FILE:-~/.dropbox_uploader}
 
 #Default chunk size in Mb for the upload process
 #It is recommended to increase this value only if you have enough free space on your /tmp partition
