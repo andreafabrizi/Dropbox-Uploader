728ea4c3fd097305aba556c422a85da50cf1c2e9
Daniil Gentili <daniil.gentili.dg@gmail.com>
2015-11-27 17:14:26 +0100

    Fixed copyright date

--- dropShell.sh
+++ dropShell.sh
@@ -2,7 +2,7 @@
 #
 # DropShell
 #
-# Copyright (C) 2013-2014 Andrea Fabrizi <andrea.fabrizi@gmail.com>
+# Copyright (C) 2013-2015 Andrea Fabrizi <andrea.fabrizi@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
