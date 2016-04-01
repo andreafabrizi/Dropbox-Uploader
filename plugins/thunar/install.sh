e85783700d1b8841a8f8a6777630e6bc3f7ea523
mDfRg <me@mindefrag.net>
2016-04-01 11:17:23 +0200

    more elegant sed solution

--- plugins/thunar/install.sh
+++ plugins/thunar/install.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-
-
 pack1=`dpkg -l |grep 'xclip' | wc -l`
 
 if [[ $pack1 = 0 ]] ; then
     echo "Please install xclip package before we continue!"
 else
 
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="thunar-dropbox.sh"
 td_sh=$DIR/$script_name
-
-
 
 rand1=$((RANDOM%1499999999999999+1400000000000000))
 rand2=$((RANDOM%29+1))
@@ -22,7 +17,6 @@ rand=$rand1'-'$rand2
 uca_xml="$HOME/.config/Thunar/uca.xml"
 
 installed=`cat $uca_xml | grep thunar-dropbox.sh | wc -l`
-
 
 if [[ $installed != 0 ]]; then
     echo "Plugin already installed!"
@@ -44,10 +38,12 @@ cat >thunarDropCustAction.txt <<EOL
 </action>
 EOL
     echo "Installing plugin..."
-    echo "someDummyText" >> $uca_xml
-    sed -n -i -e '/<\/actions>/r thunarDropCustAction.txt' -e 1x -e '2,${x;p}' -e '${x;p}' $uca_xml
-    sed -i 's/someDummyText//' $uca_xml
-    sed -i '${/^$/d;}' $uca_xml
+    
+    if lno=$(sed -n -e '/<\/actions>/=' $uca_xml)
+    then let lno=lno-1
+         sed -i -e "$lno"'r thunarDropCustAction.txt' $uca_xml
+    fi
+
     rm thunarDropCustAction.txt
     echo "Done!"
 fi
