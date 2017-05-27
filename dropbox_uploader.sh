ec91043ccce1ed41cc268830ab3361d626e7cbe7
Andrea Fabrizi <andrea.fabrizi@gmail.com>
2017-05-27 22:10:54 +0200

    Updated Copyright

--- dropbox_uploader.sh
+++ dropbox_uploader.sh
@@ -2,7 +2,7 @@
 #
 # Dropbox Uploader
 #
-# Copyright (C) 2010-2016 Andrea Fabrizi <andrea.fabrizi@gmail.com>
+# Copyright (C) 2010-2017 Andrea Fabrizi <andrea.fabrizi@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
